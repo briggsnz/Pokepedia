428d5b3293262b161e615e915b0029ec0e1729fa
Craig Briggs <cbriggsnz@gmail.com>
2017-09-02 11:05:36 +1200

    Starting Pokemon Project

---
+++ Pokemon/Pokemon/mainTableviewVC.swift
@@ -0,0 +1,37 @@
+//
+//  mainTableviewVC.swift
+//  Pokemon
+//
+//  Created by Craig Briggs on 2/09/17.
+//  Copyright © 2017 Craig Briggs. All rights reserved.
+//
+
+import UIKit
+
+class mainTableviewVC: UIViewController, UITableViewDataSource {
+
+    let array = ["one", "two", "three", "four", "five", "six"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)//UITableViewCell()
+        cell.textLabel?.text = array[indexPath.row]
+        return cell
+    }
+
+
+}
+
